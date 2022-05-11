bc1766c0844afa84d0abe77f1dc1c6f0519a2464
Samuel Jonathan <jonathandinesh@gmail.com>
2022-05-11 21:23:58 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,166 @@
+import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from 'react';
+import sinwin from "./images/sinwin.jpg";
+import './App.css';
+
+// Constants
+const TWITTER_HANDLE = 'cyka_sam';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+	'https://c.tenor.com/lseSwZ8OWxkAAAAd/rq-yay.gif',
+	'https://c.tenor.com/QGYmTuwj5-4AAAAC/cats-funny.gif',
+	'https://c.tenor.com/pYYn6TxzcEEAAAAC/cat-kitty.gif',
+	'https://c.tenor.com/JMijgjikgo4AAAAS/si-sindel.gif'
+];
+
+const App = () => {
+  // Actions
+
+  const [walletAddress, setWalletAddress] = useState(null);  
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
+  const checkIfWalletIsConnected = async () => {
+  try {
+    const { solana } = window;
+
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('Phantom wallet found!');
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log(
+          'Connected with Public Key:',
+          response.publicKey.toString()
+        );
+
+        /*
+         * Set the user's publicKey in state to be used later!
+         */
+        setWalletAddress(response.publicKey.toString());
+      }
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+  /*
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    const { solana } = window;
+  
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue('');
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  // UseEffects
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []); 
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+      
+      // Call Solana program here.
+  
+      // Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      {/* Go ahead and add this input and button to start */}
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter gif link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="cta-button submit-gif-button">Submit</button>
+      </form>
+      <div className="gif-grid">
+        {/* Map through gifList instead of TEST_GIFS */}
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+  
+  return (
+    <div className="App">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
+        <img className="sinwinpic" src={sinwin} alt="sinwin in the way" />
+        <div className="header-container">
+          <p className="header">Sinwin GIF Portal</p>
+          <p className="sub-text">
+            The only place to win-win like Sinwin ✨
+          </p>
+          {/* Add the condition to show this only if we don't have a wallet address */}
+          {!walletAddress && renderNotConnectedContainer()}
+          {/* We just need to add the inverse here! */}
+          {walletAddress && renderConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
